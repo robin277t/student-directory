e20f99e8fc479039220ee6020a247d6e45cade17
“robin277t” <robintyler87@gmail.com>
2022-10-06 15:43:09 +0000

    changes made to program

--- directory.rb
+++ directory.rb
@@ -1,16 +1,17 @@
+students = [
+ "Dr. Hannibal Lecter",
+ "Darth Vader",
+ "Nurse Ratched",
+ "Michael Corleone",
+ "Alex DeLarge",
+ "The Wicked Witch of the West",
+ "Terminator",
+ "Freddy Krueger",
+ "The Joker",
+ "Joffrey Baratheon",
+ "Norman Bates"
+ ]
 puts "The students of Villains Academy"
 puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-print "Overall, we have "
-print 11
-puts " great students"
+students.each{|stu| puts stu}
+puts "Overall, we have #{students.count} great students"
